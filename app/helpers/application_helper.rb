720e4ecc2e25133d52fcbb05895c6bf05347554f
Kelli Parris <kelli@hsdesignhouse.com>
2017-11-08 09:29:53 -0600

    scss, js, outline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def phone
+	  number_to_phone(18667504773, delimiter: ' ', area_code: true,  class: "phone-link")
+	  # number_to_phone(8667504773, delimiter: ' ', country_code: 1, area_code: true, pattern: /(\d{1})(\d{3})(\d{4})$/,  class: "phone-link")
+	end
 end
