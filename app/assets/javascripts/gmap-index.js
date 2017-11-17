aa13dbda9fd5cd56e7c6d2e6aa015ddd04cbcddf
Kelli Parris <kelli@hsdesignhouse.com>
2017-11-17 15:54:56 -0600

    try again

--- app/assets/javascripts/gmap-index.js
+++ app/assets/javascripts/gmap-index.js
@@ -16,7 +16,7 @@ document.addEventListener("turbolinks:load", function() {
          lng: location.longitude,
          title: location.address,
          infoWindow: {
-           content: `<p><a href='/locations/${location.id}'>${location.address}</a></p>`
+           content: '<p><a href='/locations/${location.id}'>${location.address}</a></p>'
          }
        });
      }
@@ -39,7 +39,7 @@ document.addEventListener("turbolinks:load", function() {
 	    var bounds = map.getBounds();
 	    var location = bounds.getSouthWest().toUrlValue() + "," + bounds.getNorthEast().toUrlValue();
 
-	    Turbolinks.visit(`/locations?l=${location}`);
+	    Turbolinks.visit('/locations?l=${location}');
 	  });
  });
 
