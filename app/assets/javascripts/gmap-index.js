873fd2a1ea1f966eb090839733bfa25072ed214b
Kelli Parris <kelli@hsdesignhouse.com>
2017-11-20 16:46:04 -0600

    test

--- app/assets/javascripts/gmap-index.js
+++ app/assets/javascripts/gmap-index.js
@@ -1,54 +1,4 @@
-document.addEventListener("turbolinks:load", function() {
-  var map = new GMaps({
-    div: '#map',
-    lat: 30.2884208,
-    lng: -81.6029046
-  });
-  window.map = map;
-
-  var locations = JSON.parse(document.querySelector("#map").dataset.locations);
-  window.locations = locations;
-
-  locations.forEach(function(location) {
-    if (location.latitude && location.longitude) {
-      var marker = map.addMarker({
-        lat: location.latitude,
-        lng: location.longitude,
-        title: location.address,
-        infoWindow: {
-          content: `<p><a href='/locations/${location.id}'>${location.address}</a></p>`
-        }
-      });
-    }
-  });
-
-  var l = document.querySelector("#map").dataset.l;
-  if (l) {
-    var latlngs = l.split(',');
-    var southWest = new google.maps.LatLng(latlngs[0], latlngs[1]);
-    var northEast = new google.maps.LatLng(latlngs[2], latlngs[3]);
-    var bounds = new google.maps.LatLngBounds(southWest, northEast);
-    map.fitBounds(bounds, 0);
-  } else {
-    map.fitZoom();
-  }
-
-  document.querySelector("#redo-search").addEventListener("click", function(e) {
-    e.preventDefault();
-
-    var bounds = map.getBounds();
-    var position = bounds.getSouthWest().toUrlValue() + "," + bounds.getNorthEast().toUrlValue();
-
-    Turbolinks.visit(`/locations?l=${position}`);
-  });
-});
-
-
-
-
-
 // document.addEventListener("turbolinks:load", function() {
-
 //   var map = new GMaps({
 //     div: '#map',
 //     lat: 30.2884208,
@@ -66,18 +16,18 @@ document.addEventListener("turbolinks:load", function() {
 //         lng: location.longitude,
 //         title: location.address,
 //         infoWindow: {
-//           content: '<p><a href="/locations/${location.id}">${location.address}</a></p>'
+//           content: `<p><a href='/locations/${location.id}'>${location.address}</a></p>`
 //         }
 //       });
 //     }
 //   });
 
-// 	var l = document.querySelector("#map").dataset.l;
+//   var l = document.querySelector("#map").dataset.l;
 //   if (l) {
-//     var latlngs   = l.split(',');
+//     var latlngs = l.split(',');
 //     var southWest = new google.maps.LatLng(latlngs[0], latlngs[1]);
 //     var northEast = new google.maps.LatLng(latlngs[2], latlngs[3]);
-//     var bounds    = new google.maps.LatLngBounds(southWest, northEast);
+//     var bounds = new google.maps.LatLngBounds(southWest, northEast);
 //     map.fitBounds(bounds, 0);
 //   } else {
 //     map.fitZoom();
@@ -89,6 +39,6 @@ document.addEventListener("turbolinks:load", function() {
 //     var bounds = map.getBounds();
 //     var position = bounds.getSouthWest().toUrlValue() + "," + bounds.getNorthEast().toUrlValue();
 
-//     Turbolinks.visit("/locations?l=${position}");
+//     Turbolinks.visit(`/locations?l=${position}`);
 //   });
 // });
