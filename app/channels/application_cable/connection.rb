dda5fa6d019e4e4503b74d66803099342f66693b
Kelli Parris <kelli@hsdesignhouse.com>
2017-12-04 16:07:23 -0600

    next theory

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -22,14 +22,6 @@ module ApplicationCable
   	# 	end
   	# end
 
-    # def find_verified_user
-    #   if verified_user = User.find_by(id: cookies.signed['user.id'])
-    #     verified_user
-    #   else
-    #     reject_unauthorized_connection
-    #   end
-    # end
-
     def find_verified_user
       # (current_user = env['warden'].user) ? current_user : reject_unauthorized_connection
       if current_user = env['warden'].user
