aca9ebf4eb15f8f2cc7ae55165cc2ece8b88eb5d
Kelli Parris <kelli@hsdesignhouse.com>
2017-12-12 16:22:44 -0600

    deleted chatbox, saving my dynos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,3 @@ class ApplicationController < ActionController::Base
 	  @boss_admin ||= User.find_by(admin: true)
   end
 end
-
-
-
-# user_signed_in?
-# current_user.admin?
-# @boss_admin ||= User.find(session[:chef_id]) if session[:chef_id]
-
-# helper_method :boss_admin, :logged_in?
-	   #  if !current_user.admin?
-		  #   flash[:danger] = "Only admin users can perform that action"
-		  #   redirect_to states_path
-		  # end
